66af2153a9e64b4f84f85af26ea2a781acb4c39a
VK <vaishakh.aravind@gmail.com>
2024-02-21 22:30:21 +0530

    add expense modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:xpense/widgets/expenses_list/expenses_list.dart';
 import 'package:xpense/models/expense_model.dart';
+import 'package:xpense/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -15,13 +16,21 @@ class _ExpensesState extends State<Expenses> {
     Expense(title: 'flutter', amount: 999, date: DateTime.now(), category: Category.work),
     Expense(title: 'movie', amount: 120, date: DateTime.now(), category: Category.leisure)
   ];
+
+  void _openAddExpenseOverlay()
+  {
+    showModalBottomSheet(context: context, builder: (ctx){
+      return NewExpense();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Xpense'),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.add))
+          IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
       body: Column(
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:xpense/models/expense_model.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+//Controller to manage input
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+//Display the date picker using built-in showDatePicker function
+  void _presentDatePicker() async{
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(context: context, firstDate: firstDate, lastDate: now);
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+/*Dispose TextEditingController after use else will remain in memory 
+even when the widget is not displayed*/
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(label: Text('Title')),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  maxLength: 10,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      prefixText: '\$', label: Text('Amount')),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(_selectedDate == null ? 'Selected Date' : formatter.format(_selectedDate!)),
+                  IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month_outlined))
+                ],
+              ))
+            ],
+          ),
+          const SizedBox(height:16),
+          Row(
+            children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values.map((category) => DropdownMenuItem(
+                value: category,
+                child: Text(category.name.toUpperCase()))).toList(), 
+                onChanged: (value)
+                {
+                  if (value == null) 
+                    {
+                      return;
+                    }
+                  setState(() 
+                  {
+                    _selectedCategory = value;
+                  });
+                }),
+                const Spacer(),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel')),
+              ElevatedButton(
+                  onPressed: () {
+                    print(
+                        '${_titleController.text}\n${_amountController.text}');
+                  },
+                  child: const Text('Save Expense'))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
