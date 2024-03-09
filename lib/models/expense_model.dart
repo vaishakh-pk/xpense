c14d6bd37b451c03b4e192d2ee2e276ba6a81491
VK <vaishakh.aravind@gmail.com>
2024-03-09 12:58:03 +0530

    model for chart

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
@@ -9,34 +8,53 @@ final formatter = DateFormat().add_yMd();
 //from uuid library to autogenerate ID
 const uuid = Uuid();
 
-enum Category
-{
-  food, travel, leisure, work
-}
+enum Category { food, travel, leisure, work }
 
-const categoryIcons = 
-{
-  Category.food : Icons.lunch_dining,
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
   Category.travel: Icons.flight_takeoff,
-  Category.leisure : Icons.movie,
-  Category.work : Icons.work
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work
 };
 
-class Expense
-{
-  Expense({required this.title,required this.amount, required this.date,required this.category}) : id =uuid.v4();
-  
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+
   final String id;
   final String title;
-  final double amount; 
+  final double amount;
   final DateTime date;
   final Category category;
 
   /*instead of using function to transform data in class, use getter.
     Getters are 
   */
-  String get formattedDate
-  {
+  String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses.where((expense) => expense.category == category).toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpense {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
   }
 }
