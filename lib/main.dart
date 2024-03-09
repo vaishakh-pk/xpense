15eb7fb6974977836320f6eaa03f9ab687e6ba73
VK <vaishakh.aravind@gmail.com>
2024-03-09 12:47:52 +0530

    dark-mode

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,24 @@ import 'package:xpense/widgets/expenses.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
+var kDarkColorScheme =
+    ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: const Color.fromARGB(255, 5, 99, 125)
+      );
+
 void main() {
   runApp(MaterialApp(
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+       elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kDarkColorScheme.primaryContainer,
+                foregroundColor: kDarkColorScheme.onPrimaryContainer)),
+    ),
     theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -22,6 +38,7 @@ void main() {
                 fontWeight: FontWeight.bold,
                 color: kColorScheme.onSecondaryContainer,
                 fontSize: 16))),
+    themeMode: ThemeMode.system,
     home: const Expenses(),
   ));
 }
