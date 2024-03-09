8d45a040e3c3f8711b864ccc1c93ffd7cbb3b0a5
VK <vaishakh.aravind@gmail.com>
2024-03-09 13:10:25 +0530

    Chart

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:xpense/widgets/chart/chart.dart';
 import 'package:xpense/widgets/expenses_list/expenses_list.dart';
 import 'package:xpense/models/expense_model.dart';
 import 'package:xpense/widgets/new_expense.dart';
@@ -11,72 +12,78 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-  final List<Expense> _registeredExpenses =
-  [
-    Expense(title: 'flutter', amount: 999, date: DateTime.now(), category: Category.work),
-    Expense(title: 'movie', amount: 120, date: DateTime.now(), category: Category.leisure)
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'flutter',
+        amount: 999,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'movie',
+        amount: 120,
+        date: DateTime.now(),
+        category: Category.leisure)
   ];
 
-  void _openAddExpenseOverlay()
-  {
+  void _openAddExpenseOverlay() {
     showModalBottomSheet(
-      isScrollControlled: true,
-      context: context, builder: (ctx){
-      return NewExpense(onAddExpense : _addExpense);
-    });
+        isScrollControlled: true,
+        context: context,
+        builder: (ctx) {
+          return NewExpense(onAddExpense: _addExpense);
+        });
   }
 
-  void _addExpense(Expense expense)
-  {
+  void _addExpense(Expense expense) {
     setState(() {
       _registeredExpenses.add(expense);
     });
   }
 
-  void _removeExpense(Expense expense)
-  {
+  void _removeExpense(Expense expense) {
     final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
 
-  ScaffoldMessenger.of(context).clearSnackBars();
-  
-  // Scaffold Messenger to showSnackbar and also incorporate undo remove action
+    ScaffoldMessenger.of(context).clearSnackBars();
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
+    // Scaffold Messenger to showSnackbar and also incorporate undo remove action
+
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
         duration: const Duration(seconds: 3),
-        action: SnackBarAction(label: 'Undo',
-        onPressed: (){setState(() {
-          _registeredExpenses.insert(expenseIndex, expense);
-        });}
-        ),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _registeredExpenses.insert(expenseIndex, expense);
+              });
+            }),
         content: const Text('Expense Deleted')));
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent =
+        Center(child: Text('No Expenses found. Start adding some'));
 
-    Widget mainContent = Center(child: Text('No Expenses found. Start adding some'));
-
-    if (!_registeredExpenses.isEmpty) 
-    {
-      mainContent =   ExpenseList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,);
+    if (!_registeredExpenses.isEmpty) {
+      mainContent = ExpenseList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
     }
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Xpense'),
         actions: [
-          IconButton(onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
         ],
       ),
       body: Column(
-        children: [
-          Text('The chart'),
-          Expanded(child: mainContent)
-        ],
+        children: [Chart(expenses: _registeredExpenses), Expanded(child: mainContent)],
       ),
     );
   }
